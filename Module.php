650488a605689220ab4258ebc06d36a4b56fc299
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-07-10 00:00:00 +0000

    Redirected to public site.

--- Module.php
+++ Module.php
@@ -2,9 +2,10 @@
 
 namespace GuestPrivateRole;
 
-use Omeka\Module\AbstractModule;
-use Omeka\Module\Manager as ModuleManager;
+use Laminas\EventManager\Event;
+use Laminas\EventManager\SharedEventManagerInterface;
 use Laminas\Mvc\MvcEvent;
+use Omeka\Module\AbstractModule;
 
 class Module extends AbstractModule
 {
@@ -52,5 +53,57 @@ class Module extends AbstractModule
                 ]
             )
         ;
+    }
+
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager): void
+    {
+        $sharedEventManager->attach(
+            '*',
+            'user.login',
+            [$this, 'handleUserLogin']
+        );
+    }
+
+    /**
+     * @see https://github.com/omeka/omeka-s/pull/1961
+     */
+    public function handleUserLogin(Event $event): void
+    {
+        /**
+         * @var \Omeka\Permissions\Acl $acl
+         * @var \Omeka\Api\Manager $api
+         * @var \Omeka\Settings\Settings $settings
+         * @var \Omeka\Settings\UserSettings $userSettings
+         * @var \Laminas\Mvc\Controller\Plugin\Url $url
+         * @var \Omeka\Api\Representation\SiteRepresentation $site
+         * @var \Omeka\Entity\User $user
+         */
+        $services = $this->getServiceLocator();
+        $acl = $services->get('Omeka\Acl');
+        if ($acl->userIsAllowed('Omeka\Controller\Admin\Index', 'browse')) {
+            return;
+        }
+
+        $url = $services->get('Omeka\Settings\User');
+        $settings = $services->get('Omeka\Settings');
+        $userSettings = $services->get('Omeka\Settings\User');
+        $user = $event->getTarget();
+        $userSettings->setTargetId($user->getId());
+
+        $defaultSite = (int) $userSettings->get('guest_site', $settings->get('default_site', 1));
+        if ($defaultSite) {
+            $api = $services->get('Omeka\ApiManager');
+            try {
+                $site = $api->read('sites', ['id' => $defaultSite])->getContent();
+                $redirectUrl = $site->siteUrl();
+            } catch (\Exception $e) {
+                $redirectUrl = $url->route('top');
+            }
+        } else {
+            $redirectUrl = $url->route('top');
+        }
+
+        $session = \Laminas\Session\Container::getDefaultManager()->getStorage();
+        $session->offsetSet('redirect_url', $redirectUrl);
     }
 }
